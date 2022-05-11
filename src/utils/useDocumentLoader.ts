bd82dafeb9391d7ce1315ca900a6d9d8640e9576
Charly BITEAU <cbiteau@beesbusy.com>
2022-05-11 11:18:35 +0200

    FIX

--- src/utils/useDocumentLoader.ts
+++ src/utils/useDocumentLoader.ts
@@ -33,7 +33,10 @@ export const useDocumentLoader = (): {
       const controller = new AbortController();
       const { signal } = controller;
 
-      fetch(documentURI, { method: "HEAD", signal }).then((response) => {
+      const headers = new Headers({
+        'Range': 'bytes=0-0',
+      });
+      fetch(documentURI, { method: "GET", headers, signal }).then((response) => {
         const contentTypeRaw = response.headers.get("content-type");
         const contentTypes = contentTypeRaw?.split(";") || [];
         const contentType = contentTypes.length ? contentTypes[0] : undefined;
